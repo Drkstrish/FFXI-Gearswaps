e30ce65d29727519adce3a506b8c4f1f426bdb28
Drkstrish <tjcrouch2010@gmail.com>
2016-06-14 13:23:04 -0400

    Update Drkstr-Gear.lua

--- Drkstr-Gear.lua
+++ Drkstr-Gear.lua
@@ -9,12 +9,10 @@ function Drkstr_shared_augmented_gear()
 --==================================--
 	
 -- Weapons
-
 -- Helios
-
 -- Taeon
-
 -- Telchine
+-- Acro
 
 --==================================--
 -- Sinister Reign gear
@@ -26,181 +24,40 @@ function Drkstr_shared_augmented_gear()
 --==================================--
 	
 -- Escha Zi'Tah weapons
-
 -- Despair
-
 -- Eschite
-
 -- Naga
-
 -- Psycloth
-
 -- Pursuer's
-
 -- Rawhide
-
 -- Vanya
 
 --==================================--
 -- Escha abjuration gear
 --==================================--
     
-    -- Adhemar
+-- Adhemar
+-- Amalric
+-- Apogee
+-- Argosy
+-- Carmine
+-- Emicho
+-- Kaykaus
+-- Lustratio
+-- Rao
+-- Ryuo
+-- Souveran
     
-    gear.AdhemarBonnet = {}
-    gear.AdhemarBonnet.B = {name="Adhemar Bonnet", augments={'STR+10','DEX+10','Attack+15'}}
     
-    gear.AdhemarJacket = {}
-    gear.AdhemarJacket.B = {name="Adhemar Jacket", augments={'STR+10','DEX+10','Attack+15'}}
-
-    gear.AdhemarKecks = {}
-    gear.AdhemarKecks.C = {name="Adhemar Kecks", augments={'AGI+10','Rng.Acc.+15','Rng.Atk.+15'}}
+--==================================--
+-- Reisenjima gear
+--==================================--
     
-    -- Amalric
+-- Weapons
+-- Chironic
+-- Herculean
+-- Merlinic
+-- Odyssean
+-- Valorous
     
-    gear.AmalricDoublet = {}
-    gear.AmalricDoublet.A = {name="Amalric Doublet", augments={'MP+60','Mag. Acc.+15','"Mag.Atk.Bns."+15'}}
-    
-    gear.AmalricGages = {}
-    gear.AmalricGages.D = {name="Amalric Gages", augments={'INT+10','Mag. Acc.+15','"Mag.Atk.Bns."+15'}}
-    
-    gear.AmalricSlops = {}
-    gear.AmalricSlops.A = {name="Amalric Slops", augments={'MP+60','Mag. Acc.+15','"Mag.Atk.Bns."+15'}}
-    
-    -- Apogee
-    
-    gear.ApogeeCrown = {}
-    gear.ApogeeCrown.B = {name="Apogee Crown", augments={'MP+60','Pet: Attack+30','Blood Pact Dmg.+7'}}
-    
-    gear.ApogeeCrownPlus1 = {}
-    gear.ApogeeCrownPlus1.A = {name="Apogee Crown +1", augments={'MP+80','Pet: "Mag.Atk.Bns."+35','Blood Pact Dmg.+8'}}
-    
-    gear.ApogeeSlacks = {}
-    gear.ApogeeSlacks.D = {name="Apogee Slacks", augments={'Pet: STR+15','Blood Pact Dmg.+13','Pet: "Dbl. Atk."+3'}}
-    
-    gear.ApogeePumps = {}
-    gear.ApogeePumps.A = {name="Apogee Pumps", augments={'MP+60','Pet: "Mag.Atk.Bns."+30','Blood Pact Dmg.+7'}}
-    
-    -- Argosy
-    
-    gear.ArgosyCelata = {}
-    gear.ArgosyCelata.A = {name="Argosy Celata", augments={'STR+10','DEX+10','Attack+15'}}
-    
-    -- Carmine
-    
-    gear.CarmineMaskPlus1 = {}
-    gear.CarmineMaskPlus1.D = {name="Carmine Mask +1", augments={'Accuracy+20','Mag. Acc.+12','"Fast Cast"+4'}}
-    
-    gear.CarmineCuissesPlus1 = {}
-    gear.CarmineCuissesPlus1.D = {name="Carmine Cuisses +1", augments={'Accuracy+20','Attack+12','"Dual Wield"+6'}}
-    
-    -- Emicho
-    
-    gear.EmichoCoronet = {}
-    gear.EmichoCoronet.C = {name="Emicho Coronet", augments={'Pet: Accuracy+15','Pet: Attack+15','Pet: "Dbl. Atk."+3'}}
-    
-    gear.EmichoHaubert = {}
-    gear.EmichoHaubert.D = {name="Emicho Haubert", augments={'Pet: HP+100','Pet: INT+15','Pet: "Regen"+2'}}
-    
-    -- Lustratio
-    
-    gear.LustratioSubligar = {}
-    gear.LustratioSubligar.B = {name="Lustratio Subligar", augments={'Accuracy+15','DEX+5','Crit. hit rate+2%'}}
-    
-    -- Ryuo
-    
-    gear.RyuoTekko = {}
-    gear.RyuoTekko.A = {name="Ryuo Tekko", augments={'STR+10','DEX+10','Accuracy+15'}}
-    
-    --==================================--
-	-- Reisenjima gear
-    --==================================--
-    
-    -- Weapons
-    
-    gear.Colada = {}
-    gear.Colada.OH = {name="Colada", augments={'"Dbl.Atk."+2','DEX+15','Accuracy+20'}}
-    
-    gear.Gada = {}
-    gear.Gada.MAB = {name="Gada", augments={'Enmity-2','Mag. Acc.+25','"Mag.Atk.Bns."+6','DMG:+2'}}
-    
-    gear.Grioavolr = {}
-    gear.Grioavolr.MAB = {name="Grioavolr", augments={'MND+14','Mag. Acc.+29','"Mag.Atk.Bns."+25'}}
-    
-    gear.Holliday = {}
-    gear.Holliday.MAB = {name="Holliday", augments={'"Mag.Atk.Bns."+20','MND+6','Rng.Atk.+22','DMG:+17'}}
-    
-    gear.Skinflayer = {}
-    gear.Skinflayer.WSD = {name="Skinflayer", augments={'Weapon skill damage +2%','STR+9','Attack+18','DMG:+16'}}
-    
-    -- Chironic
-    
-    gear.ChironicHat = {}
-    gear.ChironicHat.DA = {name="Chironic Hat", augments={'"Dbl.Atk."+3','STR+10','Accuracy+14','Attack+14'}}
-    gear.ChironicHat.MAB = {name="Chironic Hat", augments={'Mag. Acc.+21 "Mag.Atk.Bns."+21','"Resist Silence"+5','MND+10','"Mag.Atk.Bns."+15'}}
-    
-    gear.ChironicGloves = {}
-    gear.ChironicGloves.MAB = {name="Chironic Gloves", augments={'Mag. Acc.+22 "Mag.Atk.Bns."+22','"Drain" and "Aspir" potency +2','INT+1','Mag. Acc.+7','"Mag.Atk.Bns."+7'}}
-    gear.ChironicGloves.WSD = {name="Chironic Gloves", augments={'Accuracy+12 Attack+12','"Subtle Blow"+5','Weapon skill damage +7%','Mag. Acc.+9 "Mag.Atk.Bns."+9'}}
-    
-    gear.ChironicHose = {}
-    gear.ChironicHose.MAB = {name="Chironic Hose", augments={'Mag. Acc.+24 "Mag.Atk.Bns."+24','INT+10','"Mag.Atk.Bns."+7'}}
-    
-    -- Herculean
-    
-    gear.HerculeanHelm = {}
-    gear.HerculeanHelm.MAB = {name="Herculean Helm", augments={'Mag. Acc.+20 "Mag.Atk.Bns."+20','"Fast Cast"+1','INT+6','"Mag.Atk.Bns."+9'}}
-    
-    gear.HerculeanGloves = {}
-    gear.HerculeanGloves.PDT = {name="Herculean Gloves", augments={'Accuracy+19','Phys. dmg. taken -4%','DEX+5'}}
-    gear.HerculeanGloves.TA = {name="Herculean Gloves", augments={'Accuracy+16 Attack+16','"Triple Atk."+4','DEX+6','Attack+14'}}
-    gear.HerculeanGloves.Waltz = {name="Herculean Gloves", augments={'Accuracy+3','"Waltz" potency +11%','AGI+6'}}
-    
-    gear.HerculeanTrousers = {}
-    gear.HerculeanTrousers.STR = {name="Herculean Trousers", augments={'Pet: Accuracy+21 Pet: Rng. Acc.+21','Accuracy+18','Accuracy+14 Attack+14','Mag. Acc.+20 "Mag.Atk.Bns."+20'}}
-    
-    gear.HerculeanBoots = {}
-    gear.HerculeanBoots.WSD = {name="Herculean Boots", augments={'Pet: "Store TP"+4','Pet: Mag. Acc.+18','Weapon skill damage +7%','Accuracy+16 Attack+16','Mag. Acc.+8 "Mag.Atk.Bns."+8'}}
-    
-    -- Merlinic
-    
-    gear.MerlinicHood = {}
-    gear.MerlinicHood.MAB = {name="Merlinic Hood", augments={'Mag. Acc.+21 "Mag.Atk.Bns."+21','Enmity-3','VIT+2','Mag. Acc.+15','"Mag.Atk.Bns."+11'}}
-    
-    gear.MerlinicJubbah = {}
-    gear.MerlinicJubbah.DrainAspir = {name="Merlinic Jubbah", augments={'Mag. Acc.+17 "Mag.Atk.Bns."+17','"Drain" and "Aspir" potency +8','CHR+6','Mag. Acc.+10','"Mag.Atk.Bns."+14'}}
-    gear.MerlinicJubbah.MAB = {name="Merlinic Jubbah", augments={'Mag. Acc.+20 "Mag.Atk.Bns."+20','Magic Damage +13','CHR+1','Mag. Acc.+15','"Mag.Atk.Bns."+14'}}
-    
-    gear.MerlinicDastanas = {}
-    gear.MerlinicDastanas.BPD = {name="Merlinic Dastanas", augments={'Pet: Accuracy+22 Pet: Rng. Acc.+22','Blood Pact Dmg.+10','Pet: Mag. Acc.+5'}}
-    
-    gear.MerlinicShalwar = {}
-    gear.MerlinicShalwar.MAB = {name="Merlinic Shalwar", augments={'Mag. Acc.+25 "Mag.Atk.Bns."+25','Magic burst mdg.+10%','INT+4','Mag. Acc.+1'}}
-    
-    gear.MerlinicCrackows = {}
-    gear.MerlinicCrackows.MAB = {name="Merlinic Crackows", augments={'Mag. Acc.+25 "Mag.Atk.Bns."+25','CHR+3','Mag. Acc.+7','"Mag.Atk.Bns."+10'}}
-    
-    -- Odyssean
-    
-    gear.OdysseanChestplate = {}
-    gear.OdysseanChestplate.FC = {name="Odyss. Chestplate", augments={'"Fast Cast"+3','AGI+3','Mag. Acc.+11','"Mag.Atk.Bns."+15'}}
-    
-    gear.OdysseanCuisses = {}
-    gear.OdysseanCuisses.TP = {name="Odyssean Cuisses", augments={'Accuracy+24 Attack+24','DEX+7','Attack+9'}}
-    
-    -- Valorous
-    
-    gear.ValorousMask = {}
-    gear.ValorousMask.Pet ={name="Valorous Mask", augments={'Pet: Attack+30 Pet: Rng.Atk.+30','Pet: DEX+5','Pet: Accuracy+13 Pet: Rng. Acc.+13'}}
-    
-    gear.ValorousMitts = {}
-    gear.ValorousMitts.Pet = {name="Valorous Mitts", augments={'Pet: Attack+26 Pet: Rng.Atk.+26','Pet: Phys. dmg. taken -4%','Pet: Accuracy+12 Pet: Rng. Acc.+12'}}
-    gear.ValorousMitts.TP = {name="Valorous Mitts", augments={'Accuracy+20 Attack+20','"Store TP"+1'}}
-    
-    gear.ValorousHose = {}
-    gear.ValorousHose.Pet = {name="Valorous Hose", augments={'Pet: "Subtle Blow"+9','Pet: STR+14','Pet: Accuracy+15 Pet: Rng. Acc.+15','Pet: Attack+13 Pet: Rng.Atk.+13'}}
-    gear.ValorousHose.WSD = {name="Valorous Hose", augments={'Accuracy+16 Attack+16','Damage taken-1%','VIT+4','Accuracy+12','Attack+14'}}
-    
-    gear.ValorousGreaves = {}
-    gear.ValorousGreaves.Pet = {name="Valorous Greaves", augments={'Pet: Accuracy+30 Pet: Rng. Acc.+30','Pet: "Dbl. Atk."+2','Pet: STR+9','Pet: Attack+1 Pet: Rng.Atk.+1'}}
 end
