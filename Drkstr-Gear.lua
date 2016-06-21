3e5fb8cc5528ead70af90520db08c7c3b2d5c369
Drkstrish <tjcrouch2010@gmail.com>
2016-06-21 11:37:31 -0400

    Update Drkstr-Gear.lua

--- Drkstr-Gear.lua
+++ Drkstr-Gear.lua
@@ -64,12 +64,14 @@ function Drkstr_shared_augmented_gear()
 	gear.ReiveJSE.MNK = { name="Anchoret's Mantle", augments={'STR+4','DEX+3','"Subtle Blow"+3','"Counter"+2',}}
 	gear.ReiveJSE.BLM = { name="Bane Cape", augments={'Elem. magic skill +8','Dark magic skill +7','"Mag.Atk.Bns."+4',}}
 	gear.ReiveJSE.SCH = { name="Bookworm's Cape", augments={'INT+1','MND+4','Helix eff. dur. +11','"Regen" potency+7',}}
-	gear.ReiveJSE
-	gear.ReiveJSE		
-	gear.ReiveJSE
-	gear.ReiveJSE	
-	gear.ReiveJSE
-	gear.ReiveJSE		
+	gear.ReiveJSE.THF = { name="Canny Cape", augments={'DEX+4','AGI+1','"Dual Wield"+4','Crit. hit damage +1%',}}
+	gear.ReiveJSE.BLU = { name="Cornflower Cape", augments={'MP+30','DEX+2','Accuracy+2','Blue Magic skill +9',}}
+	gear.ReiveJSE.RUN = { name="Evasionist's Cape", augments={'Enmity+1','"Embolden"+7','"Dbl.Atk."+1',}}
+	gear.ReiveJSE.RDM = { name="Ghostfyre Cape", augments={'Enfb.mag. skill +7','Enha.mag. skill +1','Mag. Acc.+8',}}
+	gear.ReiveJSE.RNG = { name="Lutian Cape", augments={'STR+3','AGI+5','"Store TP"+1','"Snapshot"+3',}}
+	gear.ReiveJSE.BST = { name="Pastoralist's Mantle", augments={'STR+1 DEX+1','Pet: Accuracy+14 Pet: Rng. Acc.+14','Pet: Damage taken -5%',}}
+	gear.ReiveJSE.SAM = { name="Takaha Mantle", augments={'STR+4','"Zanshin"+3','"Store TP"+2','Meditate eff. dur. +5',}}
+	gear.ReiveJSE.NIN = { name="Yokaze Mantle", augments={'STR+2','DEX+3','Sklchn.dmg.+4%',}}
 
 --==================================--
 -- Misc. Gear
