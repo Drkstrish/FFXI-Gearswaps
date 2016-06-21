c6189079fca7d0d9034d792a9be4cf6b5a6d47a2
Drkstrish <tjcrouch2010@gmail.com>
2016-06-21 13:24:23 -0400

    Update Drkstr-Gear.lua

--- Drkstr-Gear.lua
+++ Drkstr-Gear.lua
@@ -36,6 +36,7 @@ function Drkstr_shared_augmented_gear()
 	
 	gear.Amalriclegs = {}
 	gear.Amalriclegs.A = { name="Amalric Slops", augments={'MP+60','Mag. Acc.+15','"Mag.Atk.Bns."+15'}}
+	
 -- Apogee
 -- Argosy
 -- Carmine
@@ -69,16 +70,16 @@ function Drkstr_shared_augmented_gear()
 -- Telchine
 -- Acro
 	gear.Acrohead = {}
-	gear.Acrohead.Haste = { name="Acro Helm", augments={'Accuracy+10','Haste+3','STR+1 VIT+1',}}
+	gear.Acrohead.Haste = { name="Acro Helm", augments={'Accuracy+10','Haste+3','STR+1 VIT+1'}}
 
 	gear.Acrobody = {}
-	gear.Acrobody.Pet = { name="Acro Surcoat", augments={'Pet: Accuracy+10 Pet: Rng. Acc.+10','Pet: "Regen"+3','Pet: Damage taken -4%',}}
+	gear.Acrobody.Pet = { name="Acro Surcoat", augments={'Pet: Accuracy+10 Pet: Rng. Acc.+10','Pet: "Regen"+3','Pet: Damage taken -4%'}}
 
 	gear.Acrohands = {}
-	gear.Acrohands.Pet = { name="Acro Gauntlets", augments={'Pet: Accuracy+17 Pet: Rng. Acc.+17','Pet: "Regen"+3','Pet: Damage taken -4%',}}
+	gear.Acrohands.Pet = { name="Acro Gauntlets", augments={'Pet: Accuracy+17 Pet: Rng. Acc.+17','Pet: "Regen"+3','Pet: Damage taken -4%'}}
 
 	gear.Acrofeet = {}
-	gear.Acrofeet.Pet = { name="Acro Leggings", augments={'Pet: Attack+19 Pet: Rng.Atk.+19','Pet: "Regen"+3','Pet: Damage taken -4%',}}
+	gear.Acrofeet.Pet = { name="Acro Leggings", augments={'Pet: Attack+19 Pet: Rng.Atk.+19','Pet: "Regen"+3','Pet: Damage taken -4%'}}
 	
 --==================================--
 -- Sinister Reign gear
@@ -89,27 +90,27 @@ function Drkstr_shared_augmented_gear()
 --==================================--
 
 	gear.ReiveJSE = {}
-	gear.ReiveJSE.MNK = { name="Anchoret's Mantle", augments={'STR+4','DEX+3','"Subtle Blow"+3','"Counter"+2',}}
-	gear.ReiveJSE.BLM = { name="Bane Cape", augments={'Elem. magic skill +8','Dark magic skill +7','"Mag.Atk.Bns."+4',}}
-	gear.ReiveJSE.SCH = { name="Bookworm's Cape", augments={'INT+1','MND+4','Helix eff. dur. +11','"Regen" potency+7',}}
-	gear.ReiveJSE.THF = { name="Canny Cape", augments={'DEX+4','AGI+1','"Dual Wield"+4','Crit. hit damage +1%',}}
-	gear.ReiveJSE.BLU = { name="Cornflower Cape", augments={'MP+30','DEX+2','Accuracy+2','Blue Magic skill +9',}}
-	gear.ReiveJSE.RUN = { name="Evasionist's Cape", augments={'Enmity+1','"Embolden"+7','"Dbl.Atk."+1',}}
-	gear.ReiveJSE.RDM = { name="Ghostfyre Cape", augments={'Enfb.mag. skill +7','Enha.mag. skill +1','Mag. Acc.+8',}}
-	gear.ReiveJSE.RNG = { name="Lutian Cape", augments={'STR+3','AGI+5','"Store TP"+1','"Snapshot"+3',}}
-	gear.ReiveJSE.BST = { name="Pastoralist's Mantle", augments={'STR+1 DEX+1','Pet: Accuracy+14 Pet: Rng. Acc.+14','Pet: Damage taken -5%',}}
-	gear.ReiveJSE.SAM = { name="Takaha Mantle", augments={'STR+4','"Zanshin"+3','"Store TP"+2','Meditate eff. dur. +5',}}
-	gear.ReiveJSE.NIN = { name="Yokaze Mantle", augments={'STR+2','DEX+3','Sklchn.dmg.+4%',}}
+	gear.ReiveJSE.MNK = { name="Anchoret's Mantle", augments={'STR+4','DEX+3','"Subtle Blow"+3','"Counter"+2'}}
+	gear.ReiveJSE.BLM = { name="Bane Cape", augments={'Elem. magic skill +8','Dark magic skill +7','"Mag.Atk.Bns."+4'}}
+	gear.ReiveJSE.SCH = { name="Bookworm's Cape", augments={'INT+1','MND+4','Helix eff. dur. +11','"Regen" potency+7'}}
+	gear.ReiveJSE.THF = { name="Canny Cape", augments={'DEX+4','AGI+1','"Dual Wield"+4','Crit. hit damage +1%'}}
+	gear.ReiveJSE.BLU = { name="Cornflower Cape", augments={'MP+30','DEX+2','Accuracy+2','Blue Magic skill +9'}}
+	gear.ReiveJSE.RUN = { name="Evasionist's Cape", augments={'Enmity+1','"Embolden"+7','"Dbl.Atk."+1'}}
+	gear.ReiveJSE.RDM = { name="Ghostfyre Cape", augments={'Enfb.mag. skill +7','Enha.mag. skill +1','Mag. Acc.+8'}}
+	gear.ReiveJSE.RNG = { name="Lutian Cape", augments={'STR+3','AGI+5','"Store TP"+1','"Snapshot"+3'}}
+	gear.ReiveJSE.BST = { name="Pastoralist's Mantle", augments={'STR+1 DEX+1','Pet: Accuracy+14 Pet: Rng. Acc.+14','Pet: Damage taken -5%'}}
+	gear.ReiveJSE.SAM = { name="Takaha Mantle", augments={'STR+4','"Zanshin"+3','"Store TP"+2','Meditate eff. dur. +5'}}
+	gear.ReiveJSE.NIN = { name="Yokaze Mantle", augments={'STR+2','DEX+3','Sklchn.dmg.+4%'}}
 	
 	gear.JSEcape = {}
-	gear.JSEcape.NINWS = { name="Andartia's Mantle", augments={'AGI+20','Accuracy+20 Attack+20','Weapon skill damage +10%',}}
-	gear.JSEcape.NINTP = { name="Andartia's Mantle", augments={'DEX+20','Accuracy+20 Attack+20','"Dbl.Atk."+10',}}
-	gear.JSEcape.BST = { name="Artio's Mantle", augments={'Pet: Acc.+20 Pet: R.Acc.+20 Pet: Atk.+20 Pet: R.Atk.+20','Accuracy+20 Attack+20','Pet: "Regen"+10',}}
-	gear.JSEcape.BLUcrit = { name="Rosmerta's Cape", augments={'Accuracy+5 Attack+5','Weapon skill damage +5%',}}
-	gear.JSEcape.BLUWS = { name="Rosmerta's Cape", augments={'DEX+20','Accuracy+20 Attack+20','Crit.hit rate+10',}}
-	gear.JSEcape.SAMTP = { name="Smertrios's Mantle", augments={'DEX+20','Accuracy+20 Attack+20','"Dbl.Atk."+10',}}
-	gear.JSEcape.SAMWS = { name="Smertrios's Mantle", augments={'STR+20','Accuracy+20 Attack+20','Weapon skill damage +10%',}}
-	gear.JSEcape.BLM = { name="Taranus's Cape", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','"Mag.Atk.Bns."+10',}}
+	gear.JSEcape.NINWS = { name="Andartia's Mantle", augments={'AGI+20','Accuracy+20 Attack+20','Weapon skill damage +10%'}}
+	gear.JSEcape.NINTP = { name="Andartia's Mantle", augments={'DEX+20','Accuracy+20 Attack+20','"Dbl.Atk."+10'}}
+	gear.JSEcape.BST = { name="Artio's Mantle", augments={'Pet: Acc.+20 Pet: R.Acc.+20 Pet: Atk.+20 Pet: R.Atk.+20','Accuracy+20 Attack+20','Pet: "Regen"+10'}}
+	gear.JSEcape.BLUcrit = { name="Rosmerta's Cape", augments={'Accuracy+5 Attack+5','Weapon skill damage +5%'}}
+	gear.JSEcape.BLUWS = { name="Rosmerta's Cape", augments={'DEX+20','Accuracy+20 Attack+20','Crit.hit rate+10'}}
+	gear.JSEcape.SAMTP = { name="Smertrios's Mantle", augments={'DEX+20','Accuracy+20 Attack+20','"Dbl.Atk."+10'}}
+	gear.JSEcape.SAMWS = { name="Smertrios's Mantle", augments={'STR+20','Accuracy+20 Attack+20','Weapon skill damage +10%'}}
+	gear.JSEcape.BLM = { name="Taranus's Cape", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','"Mag.Atk.Bns."+10'}}
 	
 
 --==================================--
