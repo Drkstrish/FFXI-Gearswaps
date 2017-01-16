bb40287619f310161a6edaec819b59f58f9f2d27
Drkstrish <tjcrouch2010@gmail.com>
2017-01-16 08:37:24 -0500

    Update Gear.lua

--- Gear.lua
+++ Gear.lua
@@ -239,7 +239,8 @@
 	
 	gear.Ryuohands = {}
 	gear.Ryuohands.D = { name="Ryuo Tekko", augments={'DEX+10','Accuracy+20','"Dbl.Atk."+3'}}
-		gear.Ryuolegs = {}
+	
+	gear.Ryuolegs = {}
 	gear.Ryuolegs.A = { name="Ryuo Hakama", augments={'STR+10','DEX+10','Accuracy+15'}}
 	
 	gear.Ryuofeet = {}
@@ -250,18 +251,18 @@
 	gear.Souveranbody.A = {}
 	
 	gear.Souveranhands = {}
-	gear.souveranhands.A = {}
+	gear.Souveranhands.A = {}
 	
 	gear.Souveranlegs = {}
 	gear.Souveranlegs.A {}
 	
 	gear.Souveranfeet = {}
-	gear.souveranfeet.A = {}
+	gear.Souveranfeet.A = {}
 
 --==================================--
 -- Escha gear
 --==================================--
-	
+
 -- Escha Zi'Tah weapons
 	gear.NibiruAxe = {}
 	gear.NibiruAxe.C = { name="Nibiru Tabar", augments={'Pet: Attack+25','Pet: "Mag.Atk.Bns."+15','Pet: Enmity+7'}}
@@ -388,20 +389,20 @@
 -- Helios
 -- Taeon
 	gear.Taeonhead = {}
-	gear.Taeonhead.Triple{ name="Taeon Chapeau", augments={'Accuracy+14 Attack+14','"Triple Atk."+2'}}
+	gear.Taeonhead.Triple = { name="Taeon Chapeau", augments={'Accuracy+14 Attack+14','"Triple Atk."+2'}}
 	
 	gear.Taeonbody = {}
-	gear.Taeonbody.PetAcc{ name="Taeon Tabard", augments={'Pet: Accuracy+24 Pet: Rng. Acc.+24','Pet: "Dbl. Atk."+3','Pet: Haste+5'}}
+	gear.Taeonbody.PetAcc = { name="Taeon Tabard", augments={'Pet: Accuracy+24 Pet: Rng. Acc.+24','Pet: "Dbl. Atk."+3','Pet: Haste+5'}}
 	
 	gear.Taeonhands = {}
-	gear.Taeonhands.PetAcc{ name="Taeon Gloves", augments={'Pet: Accuracy+10 Pet: Rng. Acc.+10','Pet: "Dbl. Atk."+2'}}
+	gear.Taeonhands.PetAcc = { name="Taeon Gloves", augments={'Pet: Accuracy+10 Pet: Rng. Acc.+10','Pet: "Dbl. Atk."+2'}}
 	
 	gear.Taeonlegs = {}
-	gear.Taeonlegs.Triple{ name="Taeon Tights", augments={'Accuracy+19 Attack+19','"Triple Atk."+1'}}
-	gear.Taeonlegs.Pettank{ name="Taeon Tights", augments={'Pet: Accuracy+5 Pet: Rng. Acc.+5','Pet: "Regen"+3','Pet: Damage taken -4%'}}
+	gear.Taeonlegs.Triple = { name="Taeon Tights", augments={'Accuracy+19 Attack+19','"Triple Atk."+1'}}
+	gear.Taeonlegs.Pettank = { name="Taeon Tights", augments={'Pet: Accuracy+5 Pet: Rng. Acc.+5','Pet: "Regen"+3','Pet: Damage taken -4%'}}
 	
 	gear.Taeonfeet = {}
-	gear.Taeonfeet.PetAcc{ name="Taeon Boots", augments={'Pet: Accuracy+18 Pet: Rng. Acc.+18','Pet: "Dbl. Atk."+3','CHR+9'}}
+	gear.Taeonfeet.PetAcc = { name="Taeon Boots", augments={'Pet: Accuracy+18 Pet: Rng. Acc.+18','Pet: "Dbl. Atk."+3','CHR+9'}}
 	
 -- Telchine
 -- Acro
